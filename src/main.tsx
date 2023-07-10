77a9028469529c609990a6c6776338845dd2dea3
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-07-10 16:46:33 +0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './app/styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
