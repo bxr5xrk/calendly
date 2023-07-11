b8861d5379f9f4c34810992b2087eef0d34a3576
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-07-11 14:50:57 +0300

    added husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-/* eslint-env node */
-
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -8,20 +6,31 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: true,
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'jsx-quotes': ['error', 'prefer-double'],
+    'semi-spacing': ['warn', { before: false, after: true }],
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'no-multi-spaces': ['warn', { ignoreEOLComments: true }],
+    'no-trailing-spaces': ['warn', { skipBlankLines: true }],
+    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1 }],
+    'no-whitespace-before-property': 'warn',
+    'no-console': ['warn'],
+    'no-unused-vars': ['warn', { args: 'none' }],
+    'prettier/prettier': 'warn',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+};
